0b1f9c140f12a46fe082cb128b0f6be8ef93e2f4
Bony5690 <jerrybony@yahoo.com>
2018-08-09 13:07:24 -0500

    Added order now button and order summary
    Added order now button and attached order summary modal to data coming from build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -19,6 +19,10 @@ const buildConttrols = (props) => (
        removed={() => props.ingredientRemoved(ctrl.type)}
        disabled={props.disabled[ctrl.type]}/>
      ))}
+
+     <button
+     className={classes.OrderButton}
+     disabled={!props.purchasable}>ORDER NOW</button>
   </div>
 
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -19,7 +22,24 @@ class BurgerBuilder extends Component {
      cheese: 0,
      meat: 0
    },
-   totalPrice: 4
+   totalPrice: 4,
+   purchasable: false
+ }
+
+ updatePurchaseState(ingredients) {
+   // const ingredients = {
+   //   ...this.state.ingredients
+   // }
+
+   const sum = Object.keys(ingredients)
+   .map(igKey => {
+      return ingredients[igKey] })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+
+      this.setState({purchasable: sum > 0});
+
  }
 
  addIngredientHandler = (type) => {
@@ -34,6 +54,7 @@ class BurgerBuilder extends Component {
    const oldPrice = this.state.totalPrice;
    const newPrice = oldPrice + priceDeduction;
    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+   this.updatePurchaseState(updatedIngredients);
  }
 
  removeIngredientHandler = (type) => {
@@ -51,7 +72,7 @@ class BurgerBuilder extends Component {
    const oldPrice = this.state.totalPrice;
    const newPrice = oldPrice - priceAddition;
    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
-
+   this.updatePurchaseState(updatedIngredients);
 
  }
 
@@ -67,14 +88,20 @@ class BurgerBuilder extends Component {
  }
 
 
-    return(
+    return (
       <Aux>
+
+      <Modal>
+       <OrderSummary ingredients={this.state.ingredients} />
+      </Modal>
+
         <Burger ingredients={this.state.ingredients} />
-         <BuildControls
-         ingredientAdded={this.addIngredientHandler}
-         ingredientRemoved={this.removeIngredientHandler}
-         disabled={disabledInfo}
-         price={this.state.totalPrice}/>
+            <BuildControls
+             ingredientAdded={this.addIngredientHandler}
+             ingredientRemoved={this.removeIngredientHandler}
+             disabled={disabledInfo}
+             purchasable={this.state.purchasable}
+             price={this.state.totalPrice}/>
       </Aux>
     );
   }
